bdea11e6038108df40bbd53f80737f3656b35fe8
Sergey <s.safronov1995@gmail.com>
2020-06-23 17:13:06 +0300

    Creating AVLTrees Project

---
+++ AVLTreeNode.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace AVLTree
+{
+    public class AVLTreeNode<TNode> : IComparable<TNode> where TNode : IComparable
+    {
+        AVLTree<TNode> tree;
+        AVLTreeNode<TNode> left;
+        AVLTreeNode<TNode> right;
+
+        public AVLTreeNode(TNode value, AVLTreeNode<TNode> parent, AVLTree<TNode> tree)
+        {
+            Value = value;
+            Parent = parent;
+            this.tree = tree;
+        }
+
+        public AVLTreeNode<TNode> Left
+        {
+            get
+            {
+                return left;
+            }
+            internal set
+            {
+                left = value;
+                if (left != null)
+                {
+                    left.Parent = this;
+                }
+            }
+        }
+
+        public AVLTreeNode<TNode> Right
+        {
+            get
+            {
+                return right;
+            }
+            internal set
+            {
+                right = value;
+                if (right != null)
+                {
+                    right.Parent = this;
+                }
+            }
+        }
+
+        public AVLTreeNode<TNode> Parent
+        {
+            get;
+            internal set;
+        }
+
+        public TNode Value
+        {
+            get;
+            private set;
+        }
+
+        public int CompareTo(TNode other)
+        {
+            return Value.CompareTo(other);
+        }
+
+        private int MaxChildHeight(AVLTreeNode<TNode> node)
+        {
+            if (node != null)
+            {
+                return 1 + Math.Max(MaxChildHeight(node.Left), MaxChildHeight(node.Right));
+            }
+
+            return 0;
+        }
+
+        private int RightHeight
+        {
+            get
+            {
+                return MaxChildHeight(Right);
+            }
+        }
+
+        private int LeftHeight
+        {
+            get
+            {
+                return MaxChildHeight(Left);
+            }
+        }
+
+        enum TreeState
+        {
+            Balanced,
+            LeftHeavy,
+            RightHeavy,
+        }
+
+        private TreeState State
+        {
+            get
+            {
+                if (LeftHeight - RightHeight > 1)
+                {
+                    return TreeState.LeftHeavy;
+                }
+
+                if (RightHeight - LeftHeight > 1)
+                {
+                    return TreeState.RightHeavy;
+                }
+
+                return TreeState.Balanced;
+            }
+        }
+
+        private int BalanceFactor
+        {
+            get
+            {
+                return RightHeight - LeftHeight;
+            }
+        }
+
+        internal void Balance()
+        {
+            if (State == TreeState.RightHeavy)
+            {
+                if (Right != null && Right.BalanceFactor < 0)
+                {
+                    LeftRightRotation();
+                }
+                else
+                {
+                    LeftRotation();
+                }
+            }
+            else if (State == TreeState.LeftHeavy)
+            {
+                if (Left != null && Left.BalanceFactor > 0)
+                {
+                    RightLeftRotation();
+                }
+                else
+                {
+                    RightRotation();
+                }
+            }
+        }
+
+        private void ReplaceRoot(AVLTreeNode<TNode> newRoot)
+        {
+            if (this.Parent != null)
+            {
+                if (this.Parent.Left == this)
+                {
+                    this.Parent.Left = newRoot;
+                }
+                else if (this.Parent.Right == this)
+                {
+                    this.Parent.Right = newRoot;
+                }
+            }
+            else
+            {
+                tree.Head = newRoot;
+            }
+
+            newRoot.Parent = this.Parent;
+            this.Parent = newRoot;
+        }
+
+        private void LeftRotation()
+        {
+            AVLTreeNode<TNode> newRoot = Right;
+            ReplaceRoot(newRoot);
+            Right = newRoot.Left;
+            newRoot.Left = this;
+        }
+
+        private void RightRotation()
+        {
+            AVLTreeNode<TNode> newRoot = Left;
+            ReplaceRoot(newRoot);
+            Left = newRoot.Right;
+            newRoot.Right = this;
+        }
+
+        private void LeftRightRotation()
+        {
+            Right.RightRotation();
+            LeftRotation();
+        }
+
+        private void RightLeftRotation()
+        {
+            Left.LeftRotation();
+            RightRotation();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace AVLTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AVLTree<int> oak = new AVLTree<int>();
+
+            oak.Add(7);
+            oak.Add(1);
+            oak.Add(9);
+            oak.Add(3);
+            oak.Add(10);
+            oak.Add(14);
+            oak.Add(16);
+            oak.Add(5);
+            oak.Add(2);
+            oak.Add(12);
+            oak.Add(17);
+
+            foreach (var item in oak)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(oak.Contains(4));
+            
+            Console.WriteLine(oak.Contains(5));
+
+            oak.Remove(2);
+
+            foreach (var item in oak)
+            {
+                Console.WriteLine(item);
+            }
+
+            oak.Clear();
+
+            foreach (var item in oak)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
